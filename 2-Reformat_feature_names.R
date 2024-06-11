cdd215e65ade36060ecdbbf2bce9f7408a175926
eunnaracho <32682962+eunnaracho@users.noreply.github.com>
2024-06-11 17:39:19 -0400

    Add files via upload

---
+++ 2-Reformat_feature_names.R
@@ -0,0 +1,76 @@
+#Categorize features
+#Formats feature names from Columbus to region_channel_module_category number 
+
+#data <- read.csv("C:/Users/EUCHO/OneDrive - HC-SC PHAC-ASPC/Documents/CompTox-Cell-Painting-Pipeline/2024-04-10_Feb28exp_Well-level results2.csv")
+
+#Load "List_plate_1_2.RData"
+
+test_chem_well <- rbind(as.data.frame(list_all[[1]][4]), as.data.frame(list_all[[2]][4]))
+rm(list_all)
+
+#List of features
+features <- as.data.frame(colnames(test_chem_well[-c(1:3)]))
+colnames(features) <- c("features")
+
+module <- c("Axial","Compactness","Radial","Symmetry", "Gabor", "Haralick", "SER", "Intensity", "Profile","Ratio", "Length", "Width", "Roundness", "Area")
+region <- c("Cell", "Nucleus", "Cytoplasm", "Cyto", "Membrane", "Ring")
+channel <- c("AGP", "Mito", "DNA", "RNA", "ER")
+
+features$region <- str_match(features$features, paste(region, collapse = "|"))
+features$channel <- str_match(features$features, paste(channel, collapse = "|"))
+features$module <- str_match(features$features, paste(module, collapse = "|"))
+
+#Combine Gabor, Haralick, and SER in the Texture module
+features['module'][features['module']=="Gabor"|features['module']=="Haralick"|features['module']=="SER"] <- "Texture"
+features['module'][features['module']=="Ratio"|features['module']=="Length"|features['module']=="Width"|features['module']=="Roundness"|features['module']=="Area"] <- "Morph"
+
+#Add category number
+features <- features %>%
+  group_by(region, channel, module) %>%
+  mutate(cat_group = cur_group_id()) %>%
+  ungroup()
+
+features <- features %>%
+  group_by(region, channel, module, cat_group) %>%
+  mutate(ft_no = row_number()) %>%
+  ungroup()
+
+ft_colnames <- features %>%
+  dplyr::select(region, channel, module, cat_group, ft_no)
+
+#Concatenate region, channel, and module
+ft_colnames2 <- as.data.frame(paste0(ft_colnames$region,"_",ft_colnames$channel,"_",ft_colnames$module))
+colnames(ft_colnames2) <- c("features")
+
+#Concatenate region, channel, module, and category group number
+ft_colnames <- as.data.frame(paste0(ft_colnames$region,"_",ft_colnames$channel,"_",ft_colnames$module,"_",ft_colnames$cat_group, "_", ft_colnames$ft_no))
+
+#Add chemical and concentration columns back in and combine with feature names
+#sample_info <- as.data.frame(colnames(test_chem_well[c(1:2)]))
+
+sample_info <- as.data.frame(c("Well", "Chemical", "Concentration"))
+
+colnames(ft_colnames) <- "features"
+colnames(sample_info) <- "features"
+
+fts <- rbind(sample_info, ft_colnames)
+
+#Replace the original features names with the reformatted feature names
+colnames(test_chem_well) <- t(fts) # run code up to here for BMDExpress formatting
+
+#Concatenate chemical name, plate number, and well ID in one column
+test_chem_well$Chemical <- paste0(test_chem_well$Chemical, "_",test_chem_well$Concentration, "_", test_chem_well$Well)
+test_chem_well <- dplyr::select(test_chem_well, -c(Concentration, Well))
+
+##Sets sample info as row names
+rows <- test_chem_well$Chemical
+test_chem_well <- test_chem_well[,-1]
+rownames(test_chem_well) <- rows
+
+#Features in rows and samples in columns
+test_chem_well_cat <- t(test_chem_well)
+#colnames(test_chem_well_t) <- test_chem_well_t[1,]
+test_chem_well_cat <- as.data.frame(test_chem_well_cat)
+
+test_chem_well_cat <- cbind(ft_colnames2, test_chem_well_cat)
+rownames(test_chem_well_cat) <- NULL
---
+++ 3-Principal_component_analysis.R
@@ -0,0 +1,138 @@
+#Principal Component Analysis of well-level data to reduce dimensionality
+#Global Mahalanobis 
+
+library(stats)
+library(factoextra)
+library(tcplfit2)
+
+#test_chem_well.RData as input
+
+#Principal component analysis (PCA)
+pca <- prcomp(test_chem_well[,-c(1)], center = TRUE, scale = TRUE)
+#pca_x <- pca$x
+
+#Scree plot
+scree_plot <- fviz_eig(pca, addlabels = T, ylim = c(0,75), main = paste0("Scree plot"))  
+scree_plot
+
+#Calculate the number of PCs that describe >95% variance
+cumulative_prop <- cumsum(pca$sdev^2)/sum(pca$sdev^2)
+
+PC_90 <- length(which(cumulative_prop<0.90))+1
+PC_95 <- length(which(cumulative_prop<0.95))+1
+PC_99 <- length(which(cumulative_prop<0.99))+1
+
+#Plot PCs (Nyffeler 2021 code)
+a <- length(cumulative_prop)
+  
+plot(x=1:a, y=cumulative_prop, col="gray50", pch=19, cex=0.5, type="p",
+     ylim=c(0,1), xlab="# of components", ylab="Proportion of variance retained", main="Principal components of HTPP U-2OS data")
+  #horizontal part
+  segments(x0=30, y0=0.90, x1 = PC_90, col="blue", lty='dashed')
+  segments(x0=30, y0=0.95, x1 = PC_95, col="blue", lty='solid', lwd=2)
+  segments(x0=30, y0=0.99, x1 = PC_99, col="blue", lty='dotted')
+  #vertical part
+  segments(x0=PC_90, y0=0.1, y1 = 0.90, col="blue", lty='dashed')
+  segments(x0=PC_95, y0=0.1, y1 = 0.95, col="blue", lty='solid', lwd=2)
+  segments(x0=PC_99, y0=0.1, y1 = 0.99, col="blue", lty='dotted')
+  text(x=c(PC_90, PC_95, PC_99), y=0.05, labels=c(PC_90, PC_95, PC_99), srt=90)
+  text(x=0, y=c(0.9, 0.95, 0.99),  labels=paste0(c(90,95,99), "%"), cex=0.7)
+
+#Rotation Matrix
+a <- ncol(pca$rotation)
+
+pca_x <- as.data.frame(pca$x)
+pca_x <- as.data.frame(cbind(chem = test_chem_well[,1], pca_x)) 
+
+DMSO_pc <- pca_x %>%
+  filter(grepl("DMSO", chem))
+
+DMSO_pc <- DMSO_pc[,-c(1)]
+
+DMSO_mean <- colMeans(DMSO_pc)
+
+dat <- as.matrix(test_chem_well[,-c(1)]) %*% pca$rotation[,1:PC_95]
+
+#Covariance Matrix
+Cov <- cov(dat)
+
+#Checkpoint
+det(Cov)
+isSymmetric(Cov)
+
+#Mahalanobis distance determination
+
+mahal_dist <- mahalanobis(dat, DMSO_mean, Cov, inverted = F) 
+
+mahal_dist <- as.data.frame(mahal_dist)
+
+mahal_dist <- cbind(chem = test_chem_well[,1], mahal_dist)
+
+sample <- as.data.frame(mahal_dist$chem)
+colnames(sample) <- "sample"
+
+sample <- tibble(sample) %>%
+  separate(sample, c("chem", "concentration"), sep = "_", remove = T)
+
+mahal_dist <- cbind(sample, mahal_dist = mahal_dist$mahal_dist)
+
+#Plot Mahalanobis distances 
+ggplot(mahal_dist, aes(x=concentration, y=mahal_dist, colour=chem)) +
+  geom_point() +
+  scale_y_continuous() +
+  facet_wrap(vars(chem), scales = "free_y")
+
+#####Tcplfit2 to derive benchmark concentrations#####
+
+test_chem_res <- mahal_dist %>%
+  filter(chem !="DMSO")
+
+chem_list <- unique(test_chem_res$chem)
+
+vehicle_ctrl <- mahal_dist %>%
+  filter(chem == "DMSO") %>%
+  summarise(Median = median(mahal_dist, na.rm=T), 
+            nMad = mad(mahal_dist, constant=1.4826, na.rm=T))
+
+#Function to model concentration-response using concRespCore of tcplfit2
+conc_res_modeling <- function(test_chem, vehicle_ctrl){ 
+  
+  row <- list(conc = as.numeric(test_chem$concentration),
+             resp = test_chem$mahal_dist,
+             bmed = vehicle_ctrl$Median,
+             cutoff = vehicle_ctrl$nMad,
+             onesd = vehicle_ctrl$nMad,
+             name = paste0(test_chem$chem[1]),
+             assay = "Mahalanobis distance (cutoff = 1)")
+  
+  concRespCore(row, conthits = TRUE, aicc = FALSE, force.fit = FALSE,  bidirectional = TRUE,
+            fitmodels=c("cnst", "hill", "poly1", "poly2", "pow", "exp2", "exp3","exp4", "exp5"),
+            bmr_scale = 1.349)
+  
+  }
+
+#Model concentration-response for each chemical and plot individually  
+tcpl_results <- lapply(chem_list, function(x){
+    chem_data <- test_chem_res %>%
+      filter(grepl(x, chem))
+    tcpl_chem <- conc_res_modeling(chem_data, vehicle_ctrl)
+    concRespPlot(tcpl_chem, ymin= min(chem_data$mahal_dist)-5, ymax=max(chem_data$mahal_dist)+10,  draw.error.arrows = FALSE)
+    
+    tcpl_chem
+    }) %>%
+      do.call(rbind, .)
+
+#Plot all BMC, BMCU, and BMCL together
+BMC_plot <- ggplot(tcpl_results, aes(x=bmd, y=name, colour=name)) +
+    geom_point(size=2) +
+    geom_errorbar(aes(xmin = bmdl, xmax = bmdu), width = 0.2) +
+    scale_x_log10() +
+    xlab("Median Best BMC (1SD above vehicle control) \u03BCM") +
+    ylab("Chemicals")
+  
+BMC_plot
+
+#Save tcpl results
+save(tcpl_results, file = "Global fitting Mahalanobis - tcplResult_cutoff_1.RData")
+
+
